0b151e46207c2364bd3bfcecac334a2f36325f5e
unknown <abianmoksa25@gmail.com>
2025-12-06 19:51:36 +0700

    save sblm merge

--- Tugas-Besar-IF4803-Kelompok_09/dokter.h
+++ Tugas-Besar-IF4803-Kelompok_09/dokter.h
@@ -23,10 +23,9 @@ struct DokterList {
     adrDokter first;
 };
 
-//dokter
+//insert & delete //kerjain ini
 void createListDokter(DokterList &L);
 adrDokter createElmDokter(InfoDokter x);
-//insert & delete
 void insertFirstD(DokterList &L, adrDokter P);
 void insertLastD(DokterList &L, adrDokter P);
 void insertAfterD(adrDokter Prec, adrDokter P);
@@ -34,6 +33,7 @@ void deleteFirstD(DokterList &L, adrDokter &P);
 void deleteLastD(DokterList &L, adrDokter &P);
 void deleteAfterD(adrDokter Prec, adrDokter &P);
 
+//ini minggu depan
 void insertDokterByCondition(DokterList &L, adrDokter P);
 void deleteDokterByCondition(DokterList &L, int idDokter);
 void addPasienToDokterByCondition(adrDokter dokter, adrPasien pasien);
@@ -42,6 +42,7 @@ int countTotalPasienAllDokter(DokterList L);
 adrDokter searchDokterBySpesialisasi(DokterList L, string spesialisasi);
 void showAllDokter(DokterList L);
 void showAllPasienUnik(DokterList L);
+//ini minggu depan
 //buat realasi
 adrDokter searchDokter(DokterList L, int idDokter);
 void addPasienToDokter(adrDokter dokter, adrPasien pasien);
